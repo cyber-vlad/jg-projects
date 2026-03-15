3bf18ba113b149178b7f4292a735f99163c39b76
cyber-vlad <v.lysyi2003vlad@gmail.com>
2026-03-15 21:18:51 +0200

    feat: add models

---
+++ MessageBroker/MessageBroker.Common/Models/SubscriptionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MessageBroker.Common.Models
+{
+    [Serializable]
+    [DataContract]
+    public class SubscriptionResponse
+    {
+        [DataMember]
+        public string Topic { get; set; }
+        [DataMember]
+        public string QueuePath { get; set; }
+    }
+}
