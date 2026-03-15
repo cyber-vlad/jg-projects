dc16101298c5203e1f22e90367246d148721d3fd
cyber-vlad <v.lysyi2003vlad@gmail.com>
2026-03-15 21:14:40 +0200

    feat: add initial project

---
+++ MessageBroker/MessageBroker.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageBroker.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ MessageBroker/MessageBroker.Host/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageBroker.Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
